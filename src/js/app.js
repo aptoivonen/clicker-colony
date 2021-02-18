14e0af610fc9157b5b2bf565bb4a6272c8b4816d
Aleksi Toivonen <aptoivonen@mail.com>
2021-02-18 11:22:46 +0200

    Start colony logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 import Button from "../../node_modules/bootstrap/js/dist/button";
-import { test } from "./colony";
+import Colony from "./colony";
 
-test();
+const colony = new Colony();
--- src/js/colony.js
+++ src/js/colony.js
@@ -1,3 +1,26 @@
-export const test = () => {
-  console.log("Test!");
+const resources = ["coins", "colonists", "power", "copper", "lead"];
+
+const getDomElements = (resourceId) => {
+  return {
+    amount: document.getElementById(resourceId + "-amount"),
+    change: document.getElementById(resourceId + "-change"),
+    button: document.getElementById(resourceId + "-button"),
+  };
 };
+
+const initResource = (resourceObj) => {
+  resourceObj.amount.textContent = "0";
+  resourceObj.change.textContent = "+ 0";
+  resourceObj.button.addEventListener("click", (event) => {
+    console.log("clicked: " + event.currentTarget.id);
+  });
+};
+
+export default class Colony {
+  constructor() {
+    resources.forEach((resourceId) => {
+      this[resourceId] = getDomElements(resourceId);
+      initResource(this[resourceId]);
+    });
+  }
+}
