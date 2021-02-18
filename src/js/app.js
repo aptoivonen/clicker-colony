1cdc70131c89b4d72012818a1fb9d31d08c217b1
Aleksi Toivonen <aptoivonen@mail.com>
2021-02-18 12:21:59 +0200

    Fix Bootstrap's js imports

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
-import Button from "../../node_modules/bootstrap/js/dist/button";
+import "../../node_modules/bootstrap/js/dist/button";
+import "../../node_modules/bootstrap/js/dist/tab";
 import Colony from "./colony";
 
 const colony = new Colony();
