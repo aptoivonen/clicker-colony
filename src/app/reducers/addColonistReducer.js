3b40c22329afb2a9ad89194d555ca5771c8734d5
Aleksi Toivonen <aptoivonen@mail.com>
2022-01-05 21:31:45 +0200

    Make addColonistReducer to check capacity

--- src/app/reducers/addColonistReducer.js
+++ src/app/reducers/addColonistReducer.js
@@ -1,12 +1,23 @@
-import { hasResources, decrementResources } from "app/utils";
+import {
+  hasResources,
+  decrementResources,
+  getAllColonists,
+  getColonistCapacity,
+} from "app/utils";
 
+/**
+ * Adds a new colonist if there are enough resources to pay for him and enough capacity to hold him.
+ * @param {{}} state the global state
+ */
 const addColonistReducer = (state) => {
   const {
     resources,
     colonists: { hireCost },
   } = state;
 
-  const canAddColonist = hasResources(hireCost, resources);
+  const canAddColonist =
+    hasResources(hireCost, resources) &&
+    getColonistCapacity(state) > getAllColonists(state);
 
   if (canAddColonist) {
     state.resources = decrementResources(hireCost, resources);
