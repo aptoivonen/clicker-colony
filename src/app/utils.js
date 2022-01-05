1980966da367dd676c5bfbd69872e4d2149a0a4c
Aleksi Toivonen <aptoivonen@mail.com>
2022-01-05 13:37:42 +0200

    Create some app utilities

---
+++ src/app/utils.js
@@ -0,0 +1,18 @@
+export const hasResources = (resourcesRequirementObj, targetResourcesObj) =>
+  Object.entries(resourcesRequirementObj).every(
+    ([entryKey, entryValue]) =>
+      entryValue >= 0 && targetResourcesObj[entryKey] >= entryValue
+  );
+
+export const decrementResources = (
+  resourcesRequirementObj,
+  targetResourcesObj
+) => {
+  const newTargetResourcesObj = Object.fromEntries(
+    Object.entries(targetResourcesObj).map(([entryKey, entryValue]) => [
+      entryKey,
+      Math.max(entryValue - (resourcesRequirementObj[entryKey] || 0), 0),
+    ])
+  );
+  return newTargetResourcesObj;
+};
