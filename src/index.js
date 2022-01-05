44e7d0892e7987548f06d8aa1f1889313d33eab0
Aleksi Toivonen <aptoivonen@mail.com>
2022-01-05 13:40:19 +0200

    Start the updates in index.js
    Will tick every second. Maybe another place for this might be better?

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { update } from "app/rootReducer";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,3 +14,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// Start the updates once per second
+setInterval(() => {
+  store.dispatch(update());
+}, 1000);
