f22f18d678d6604ccf5e8f307e0dfa27857af9f5
Aleksi Toivonen <aptoivonen@mail.com>
2022-01-07 20:31:52 +0200

    Add comment to action creators

--- src/app/actionCreators.js
+++ src/app/actionCreators.js
@@ -1,5 +1,10 @@
 import { createAction } from "@reduxjs/toolkit";
 
+/**
+ * Meant to be used in thunks, not directly in components.
+ * Components should only use thunks.
+ */
+
 const updated = createAction("updated");
 const colonistAdded = createAction("colonistAdded");
 const resourceClicked = createAction("resourceClicked");
