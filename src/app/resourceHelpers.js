71caf058fd615a129f4bca122ea49e5df65308cd
Aleksi Toivonen <aptoivonen@mail.com>
2022-01-07 14:00:23 +0200

    Rewrite jsdoc

--- src/app/resourceHelpers.js
+++ src/app/resourceHelpers.js
@@ -1,21 +1,13 @@
 /**
  * Decrements resources by mutating ResourceState object since there is Immer underneath.
+ * Resource values will remain >= 0 after this.
  * @param {{}} resourceState resources object
  * @param {{}} resourceRequirement resources to decrement
- * @returns {{}} the result of the decrement
  */
 export const decrementResources = (resourceState, resourceRequirement) => {
   Object.entries(resourceState).forEach(([key, value]) => {
     resourceState[key] = Math.max(value - (resourceRequirement[key] || 0), 0);
   });
-
-  // const newState = Object.fromEntries(
-  //   Object.entries(state).map(([entryKey, entryValue]) => [
-  //     entryKey,
-  //     Math.max(entryValue - (resourceRequirement[entryKey] || 0), 0),
-  //   ])
-  // );
-  // return newState;
 };
 
 /**
